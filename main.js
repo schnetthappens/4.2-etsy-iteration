25bdfadda07a92c8b5c3e007ee80af89cf5b8649
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-26 16:55:07 -0400

    Console log filtered prices, finish number two

--- main.js
+++ main.js
@@ -21,8 +21,30 @@ var items = require('./json/items.json');
   //   console.log(averagePrice);
   // }
 
+  //EXERCISE 1 — Print the average price to the console
+
   /*Print the average price to the console*/
 var average = _.reduce(items, function(total, item) {
   return total + item.price;
 }, 0) / items.length;
 console.log(average);
+
+
+
+//EXERCISE 2 — Console log an array of items between $14-$18
+
+/*
+WHAT DO I KNOW
+I already have an array of objects
+Each of those objects has a price value
+Need to have certain objects (based on their price) returned to me
+The returned array will be shorter than the length of the original array
+*/
+
+var filterPrice = _.filter(items, function(returnProducts) {
+  return Number (returnProducts.price) > 14 && (returnProducts.price) < 18;
+});
+
+filterPrice.forEach(function(item) {
+  console.log(item.title);
+});
