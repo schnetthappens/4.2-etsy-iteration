9fcccc31a89c272d73c6ac66959f9663021aac95
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-05-26 16:05:42 -0400

    Console log average price, finish number one

--- main.js
+++ main.js
@@ -3,11 +3,26 @@ var _ = require('underscore');
 var items = require('./json/items.json');
 
 
-/*
-Need to get price from all of the objects in the items array
-*/
+  // /*Get price value from all of the objects in the items array*/
+  // var getPrices = _.map(items, function(item)  {
+  //   return item.price;
+  // });
+  //
+  //
+  // /*Get the sum of the prices returned from the mapped array*/
+  // var sumOfPrices = _.reduce(getPrices, function(a, b) {
+  //   return a + b;
+  // });
+  //
+  //
+  // // /*Print the average price to the console*/
+  // function getAveragePrice(sumOfPrices, getPrices) {
+  //   var averagePrice = sumOfPrices / items.length;
+  //   console.log(averagePrice);
+  // }
 
-var findPrice = _.map(items, function(item)  {
-  // return item.price;
-  console.log(item.price);
-});
+  /*Print the average price to the console*/
+var average = _.reduce(items, function(total, item) {
+  return total + item.price;
+}, 0) / items.length;
+console.log(average);
